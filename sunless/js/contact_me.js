6007a652814b2440344b09f236e9fcceea11a1cb
ahoward87 <ahoward87@fullsail.edu>
2014-12-11 20:14:43 -0600

    form

--- sunless/js/contact_me.js
+++ sunless/js/contact_me.js
@@ -50,7 +50,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it looks like something went wrong. Please contact me at ashley@sensationallysunless.com for help. Thank you!</strong>");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
